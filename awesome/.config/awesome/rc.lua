7c697d4e932f2a3703f42101c8f50d570e6885bc
Pekka Ervasti <pekka.ervasti@haltian.com>
2021-12-01 22:49:43 +0200

    alacritty: move to proper directory
    Signed-off-by: Pekka Ervasti <pekka.ervasti@haltian.com>

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -57,7 +57,10 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+
+-- terminal = "x-terminal-emulator"
+terminal = "alacritty"
+
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
