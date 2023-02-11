7d8e6d3e4442591f6d874bb11362bed364bd4343
Pekka Ervasti <peervast@gmail.com>
2023-02-11 14:15:39 +0200

    emacs: add tree-sitter grammar helpers
    Signed-off-by: Pekka Ervasti <peervast@gmail.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -810,15 +810,15 @@
 
 (add-to-list 'load-path "~/.my-emacs.d")
 (require 'my-user-config)
-(require 'my-erc-sasl-config)
+;;(require 'my-erc-sasl-config)
 
 (add-hook 'edebug-mode-hook 'evil-normalize-keymaps)
 (add-hook 'dart-mode-hook 'lsp)
 
 ;; EAF
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
-(require 'eaf)
-(require 'eaf-pdf-viewer)
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+;; (require 'eaf)
+;; (require 'eaf-pdf-viewer)
 
 ;; EAF evil
 (define-key key-translation-map (kbd "SPC")
@@ -943,3 +943,33 @@
 ;;   )
 
 ;; (add-hook 'logview-mode-hook 'pe/logview-activate-map)
+
+(setq treesit-language-source-alist
+      '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+	(c . ("https://github.com/tree-sitter/tree-sitter-c"))
+	(cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+	(css . ("https://github.com/tree-sitter/tree-sitter-css"))
+	(go . ("https://github.com/tree-sitter/tree-sitter-go"))
+	(html . ("https://github.com/tree-sitter/tree-sitter-html"))
+	(javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+	(json . ("https://github.com/tree-sitter/tree-sitter-json"))
+	(lua . ("https://github.com/Azganoth/tree-sitter-lua"))
+	(make . ("https://github.com/alemuller/tree-sitter-make"))
+	(ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" "ocaml/src" "ocaml"))
+	(python . ("https://github.com/tree-sitter/tree-sitter-python"))
+	(php . ("https://github.com/tree-sitter/tree-sitter-php"))
+	(typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "typescript/src" "typescript"))
+	(ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+	(rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+	(sql . ("https://github.com/m-novikov/tree-sitter-sql"))
+	(toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+	(zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+
+(defun nf/treesit-install-all-languages ()
+  "Install all languages specified by `treesit-language-source-alist'."
+  (interactive)
+  (let ((languages (mapcar 'car treesit-language-source-alist)))
+    (dolist (lang languages)
+      (treesit-install-language-grammar lang)
+      (message "`%s' parser was installed." lang)
+      (sit-for 0.75))))
