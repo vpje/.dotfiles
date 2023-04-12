3a7d31c14b31eaa4ee26374ad67a48471faeac86
Pekka Ervasti <pekka.ervasti@haltian.com>
2023-04-12 14:49:46 +0300

    emacs: use-packages repo, yaml-mode and rustic

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1009,3 +1009,28 @@ argument the push-remote can be changed before pushed to it."
 
 (transient-append-suffix 'magit-push "p"
   '("g" magit-push-current-for-gerrit-review))
+
+(use-package repo :ensure t)
+
+(use-package yaml-mode :ensure t)
+
+(use-package rustic
+  :ensure
+  ;; :bind (:map rustic-mode-map
+  ;;             ("M-j" . lsp-ui-imenu)
+  ;;             ("M-?" . lsp-find-references)
+  ;;             ("C-c C-c l" . flycheck-list-errors)
+  ;;             ("C-c C-c a" . lsp-execute-code-action)
+  ;;             ("C-c C-c r" . lsp-rename)
+  ;;             ("C-c C-c q" . lsp-workspace-restart)
+  ;;             ("C-c C-c Q" . lsp-workspace-shutdown)
+  ;;             ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  )
