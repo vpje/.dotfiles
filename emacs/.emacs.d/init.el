9c7783b4fed65871dec0d5e3a865e6a310119b6f
Pekka Ervasti <pekka.ervasti@haltian.com>
2024-05-18 21:05:37 +0300

    emacs: winum change window with M-[number]

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -382,6 +382,19 @@
 
 (use-package winum
   :ensure t
+  :init
+  (defvar-keymap winum-keymap
+    :doc "Keymap for winum-mode actions."
+    "M-0" 'winum-select-window-0-or-10
+    "M-1" 'winum-select-window-1
+    "M-2" 'winum-select-window-2
+    "M-3" 'winum-select-window-3
+    "M-4" 'winum-select-window-4
+    "M-5" 'winum-select-window-5
+    "M-6" 'winum-select-window-6
+    "M-7" 'winum-select-window-7
+    "M-8" 'winum-select-window-8
+    "M-9" 'winum-select-window-9)
   :config
   (winum-mode 1)
   (pe/leader-def
@@ -396,7 +409,8 @@
     "7" 'winum-select-window-7
     "8" 'winum-select-window-8
     "9" 'winum-select-window-9
-    ))
+    )
+  )
 
 (use-package magit-section)
 
