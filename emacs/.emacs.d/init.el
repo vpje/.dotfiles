2a7103f308f6ccc1cf54e6b9cca83075f123d0e7
Pekka Ervasti <peervast@gmail.com>
2025-02-28 08:15:36 +0200

    emacs: add treesitter-context-mode hook for c++

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1406,6 +1406,7 @@
 (add-to-list 'load-path "~/.emacs.d/git/treesitter-context.el/")
 (require 'treesitter-context)
 (add-hook 'c-ts-mode-hook #'treesitter-context-mode)
+(add-hook 'c++-ts-mode-hook #'treesitter-context-mode)
 (add-hook 'python-ts-mode-hook #'treesitter-context-mode)
 
 (message "End of init.el")
