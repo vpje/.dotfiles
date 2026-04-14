436260b437545668f2bbfbf8624cb62df8d59b16
Pekka Ervasti <pekka.ervasti@haltian.com>
2026-04-14 13:45:27 +0300

    awesome: default term to ghostty

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -59,7 +59,8 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 -- This is used later as the default terminal and editor to run.
 
 -- terminal = "x-terminal-emulator"
-terminal = "alacritty"
+-- terminal = "alacritty"
+terminal = "ghostty"
 
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
